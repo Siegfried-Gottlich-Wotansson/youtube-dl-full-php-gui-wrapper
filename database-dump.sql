68f1c72c7e96e7625cf5b3f895e227da7bd10c9d
Dante <p.ionut196@gmail.com>
2017-09-20 14:55:37 -0400

    updated database sql

--- database-dump.sql
+++ database-dump.sql
@@ -1,17 +1,65 @@
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Sep 20, 2017 at 02:55 PM
+-- Server version: 10.0.31-MariaDB-0ubuntu0.16.04.2
+-- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
--- ----------------------------
--- Table structure for fisiere
--- ----------------------------
-DROP TABLE IF EXISTS `fisiere`;
-CREATE TABLE `fisiere`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `externalid` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-  `locatie` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-  `timestamp` datetime(0) DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP(0),
-  `accesari` int(255) DEFAULT 0,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-SET FOREIGN_KEY_CHECKS = 1;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ytstored`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fisiere`
+--
+
+CREATE TABLE `fisiere` (
+  `id` int(11) NOT NULL,
+  `externalid` varchar(255) DEFAULT NULL,
+  `file_name` varchar(255) DEFAULT NULL,
+  `timestamp` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `accesari` int(255) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `fisiere`
+--
+
+INSERT INTO `fisiere` (`id`, `externalid`, `file_name`, `timestamp`, `accesari`) VALUES
+(96, 'FPRbeFRMI3g', 'Spike - amintiri', '2017-09-20 14:52:26', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fisiere`
+--
+ALTER TABLE `fisiere`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fisiere`
+--
+ALTER TABLE `fisiere`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
